222128c28783a578bfb1584c1d8bd0474c7e07fa
Jianwei Sun <jianwei.sun@gmail.com>
2011-09-29 22:51:16 -0400

    add object class

---
+++ CoreDataTest/Object.m
@@ -0,0 +1,19 @@
+//
+//  Object.m
+//  CoreDataTest
+//
+//  Created by Jianwei Sun on 9/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Object.h"
+#import "Category.h"
+
+
+@implementation Object
+@dynamic name;
+@dynamic imagePath;
+@dynamic category;
+
+
+@end
